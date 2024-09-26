878c41ecef7580c04b2493c024951e5f80f0b1b1
Robert De Leon <68812513+bobbybwoy@users.noreply.github.com>
2024-09-26 17:19:19 +0100

    Added tests

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -12,6 +12,41 @@ public class GildedRoseTest
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].Name, Is.EqualTo("foo"));
+    }
+
+    [Test]
+    public void Sulfuras()
+    {
+        string itemName = "Sulfuras, Hand of Ragnaros";
+
+        var items = new List<Item> { new Item { Name = itemName, SellIn = 10, Quality = 10 }};
+        var app = new GildedRose(items);
+
+        app.UpdateQuality();
+    }
+
+    [Test]
+    public void AgedBrie()
+    {
+        string itemName = "Aged Brie";
+    }
+
+    [Test]
+    public void BackstagePass()
+    {
+        string itemName = "Backstage passes to a TAFKAL80ETC concert";
+    }
+
+    [Test]
+    public void AnyOtherItem()
+    {
+       string itemName = "Tulip"; 
+    }
+
+    [Test]
+    public void SpecialItem()
+    {
+       string itemName = "Tulip"; 
     }
 }
